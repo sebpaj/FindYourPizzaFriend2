4c879e90835ec9eb4e8ba55c75947126e4def723
sebpaj <sseba07@gmail.com>
2022-08-13 20:57:07 +0200

    Connecting client with api-server

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { trpc } from "./trpc";
 
 import "./index.css";
 
-const App = () => (
-  <div className="container">
-    <div>Name: client</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Empty CSS</div>
-  </div>
-);
+const client = new QueryClient();
+
+const AppContent = () => {
+  const hello = trpc.useQuery(["hello"]);
+  return <div>YO {hello?.data}</div>;
+};
+
+const App = () => {
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      url: "http://localhost:8080/trpc",
+    })
+  );
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={client}>
+      <QueryClientProvider client={client}>
+        <AppContent />
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+};
 ReactDOM.render(<App />, document.getElementById("app"));
